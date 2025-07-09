1e3c985e9315f02a70c866d9a04c6fbd40ec628a
Thai Vo <tahrbo@gmail.com>
2025-07-09 16:55:32 -0500

    Starting

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LINQ
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var franchises = new List<string>() { "Mario", "Zelda", "Donkey Kong", "Metal Gear Solid", "Assassin's Creed", "Battlefield", "Call of Duty", "Halo", "Castlevania", "The Witcher", "Baldur's Gate", "Dragon Age", "Mass Effect", "Fortnite", "Final Fantasy", "Dragon Quest", "Persona", "Fallout", "Fire Emblem", "Skyrim", "God of War", "Horizon Zero Dawn", "Bravely Default", "Shin Megami Tensei", "Xenoblade Chronicles", "Tales"};
+        
+            var orderedByLength = franchises.OrderBy(x => x.Length).ToList();
+
+            foreach (var franchise in orderedByLength) 
+            { 
+                Console.WriteLine(franchise);
+            }
         }
     }
 }
